03227d1d12898765404911eaa0b57b67098548da
rajnikantkori395 <rajnikantkori395@gmail.com>
2022-09-06 15:08:47 +0530

    all done

--- server/models/pagination.js
+++ server/models/pagination.js
@@ -1,46 +1,45 @@
 import client from "../connect.js"
 
-  
-  function paginatedResults() {
-    return async (req, res, next) => {
-      const page = parseInt(req.query.page)
-      const limit = parseInt(req.query.limit)
-  
-      const startIndex = (page - 1) * limit
-      const endIndex = page * limit
-  
-      const results = {}
-  
-      if (endIndex < (await client.query(`SELECT count(*) FROM student`)).rows[0].count) {
-        results.next = {
-          page: page + 1,
-          limit: limit
-        }
-      }
-      
-      if (startIndex > 0) {
-        results.previous = {
-          page: page - 1,
-          limit: limit
-        }
-      }
-      try {
-        const student = await client.query(`SELECT *
 
-        FROM student
-    
-         ORDER BY student.studentid 
-    
-         LIMIT  ${limit}   OFFSET ${page-1}`) 
-        // results.results = student.rows
+function paginatedResults() {
+  return async (req, res, next) => {
+    const page = parseInt(req.query.page)
+    const limit = parseInt(req.query.limit)
 
-    results.results = student.rows
-        res.paginatedResults = results
-        next()
-      } catch (e) {
-        res.status(500).json({ message: e.message })
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+
+    const results = {}
+    let offset
+    if (page == 1) {
+      offset = 0
+    }
+    else {
+      offset = (page - 1) * limit
+    }
+    if (endIndex < (await client.query(`SELECT count(*) FROM student`)).rows[0].count) {
+      results.next = {
+        page: page + 1,
+        limit: limit
       }
     }
-  }
 
-  export default paginatedResults
+    if (startIndex > 0) {
+      results.previous = {
+        page: page - 1,
+        limit: limit
+      }
+    }
+    try {
+      const student = await client.query(`SELECT * FROM student  ORDER BY student.studentid LIMIT  ${limit}   OFFSET ${offset}`)
+      // results.results = student.rows
+      results.results = student.rows
+      res.paginatedResults = results
+      next()
+    } catch (e) {
+      res.status(500).json({ message: e.message })
+    }
+  }
+}
+
+export default paginatedResults
